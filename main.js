a90031880593673d16bd73393649d3c838a5b106
Faizat Hazeez <99651410+Faidunni@users.noreply.github.com>
2022-06-25 00:31:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let myName = "Hazeez Faizat"
+let myHeight = 143
+let myCountry = "Nigeria"
+
+console.log("My name is " + myName);
+console.log("My height is "+myHeight)
+console.log("I'm from "+ myCountry)
