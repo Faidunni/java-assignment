1540a493bd760d69bd51e5375d2391682705fb8c
Faizat Hazeez <99651410+Faidunni@users.noreply.github.com>
2022-06-25 05:13:05 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var myName = "Hazeez Faizat"
+var myName = "Hazeez Faizat."
 var myHeight = 143
-var myCountry = "Nigeria"
+var myCountry = "Nigeria."
 
-document.write ("My name is " + myName);
-document.write ("My height is "+ myHeight + "cm")
-document.write ("I'm from "+ myCountry)
+document.write (" My name is " + myName);
+document.write("<br>")
+document.write (" My height is "+ myHeight + " cm. ")
+document.write("<br>")
+document.write (" I'm from "+ myCountry)
